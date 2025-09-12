c69aed2403661e2ab878a675856285b740bb53c7
Kaushik Barman <kaushikbarman449@gmail.com>
2025-09-12 17:06:25 +0530

    Implement selection sort algorithm

---
+++ striver_a2z_dsa/sorting_techniques/selection_sort.py
@@ -0,0 +1,25 @@
+# selection sort
+
+def swapValues(first, second):
+    return second, first
+
+
+def selection_sort(sortList):
+    for j in range(len(sortList)):
+        smallestElement = j
+        for i in range(j+1, len(sortList)):
+            if sortList[i] < sortList[smallestElement]:
+                smallestElement = i
+
+        if smallestElement != j:
+            sortList[j], sortList[smallestElement] = swapValues(
+                sortList[j], sortList[smallestElement])
+
+    return sortList
+
+
+if __name__ == "__main__":
+    sortList = [1, 4, 3, 11, 3, -13, 3, 0, 99]
+    print("Original list", sortList)
+    selection_sort(sortList)
+    print("Sorted list", sortList)
