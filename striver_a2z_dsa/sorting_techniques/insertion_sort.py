036f43dad951d868f785f9c80b9bbe453176c299
Kaushik Barman <kaushikbarman449@gmail.com>
2025-09-14 17:42:39 +0530

    Refactor insertion sort for improved readability by adjusting line breaks in swap operation

--- striver_a2z_dsa/sorting_techniques/insertion_sort.py
+++ striver_a2z_dsa/sorting_techniques/insertion_sort.py
@@ -5,8 +5,7 @@ def insertion_sort(sortList):
     for i in range(1, n - 1):
         pointer = i
         while pointer > 0 and sortList[pointer - 1] > sortList[pointer]:
-            sortList[pointer -
-                     1], sortList[pointer] = sortList[pointer], sortList[pointer - 1]
+            sortList[pointer - 1], sortList[pointer] = sortList[pointer], sortList[pointer - 1]
             pointer -= 1
     return sortList
 
