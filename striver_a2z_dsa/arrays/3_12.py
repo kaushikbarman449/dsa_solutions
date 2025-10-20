918ae72c4cb5c4817787a8e90cb5f2fcf0e26208
Kaushik Barman <kaushikbarman449@gmail.com>
2025-10-20 11:56:40 +0530

    Add implementation for Longest Subarray with given Sum K in O(n) time complexity

--- striver_a2z_dsa/arrays/3_12.py
+++ striver_a2z_dsa/arrays/3_12.py
@@ -24,10 +24,32 @@ def longest_subArray_O2(nums, sum_k):
     return subArray_len
 
 
+def longest_subArray_On(nums, sum_k):
+    prefix_map = {}
+    prefix_sum = 0
+    max_len = 0
+
+    for i in range(len(nums)):
+        prefix_sum += nums[i]
+
+        if prefix_sum == sum_k:
+            max_len = i + 1
+
+        if (prefix_sum - sum_k) in prefix_map:
+            max_len = max(max_len, i - prefix_map[prefix_sum - sum_k])
+
+        if prefix_sum not in prefix_map:
+            prefix_map[prefix_sum] = i
+
+    return max_len
+
+
 if __name__ == "__main__":
     sum_k = int(input("Enter k: "))
     list_values = [2, 3, 9, 5, 1, 3, 1, 9, 0, 0, 0, 0]
     print(f"Length of longest subarray with {sum_k} in O(3):", longest_subArray_O3(
         list_values, sum_k))
     print(f"Length of longest subarray with {sum_k} in O(2):", longest_subArray_O2(
+        list_values, sum_k))
+    print(f"Length of longest subarray with {sum_k} in O(n):", longest_subArray_On(
         list_values, sum_k))
